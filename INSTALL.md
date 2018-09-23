7d498526823e7e0f4e596bf5539102bddaa16b74
Simon Lopez <s.lopez@brgm.fr>
2018-09-23 21:49:22 +0200

    Initial import from ComPASS

---
+++ INSTALL.md
@@ -0,0 +1,93 @@
+## Requirements
+
+### Compilers
+
+You will need a relatively recent C++-14 compiler.
+
+Compilation has been successfully tested with:
+  * gcc-6.3 and gcc-7.2 on Linux
+  * Intel icpc-17 on Linux
+  * Microsoft Visual Studio 2015 (i.e. version 14.0) on Windows
+
+### Build system
+
+You will need a recent version of [CMake](https://cmake.org/).
+Version 3.4 or above should be ok.
+
+The windows installer that you will find in
+the [download section of the cmake site](https://cmake.org/download/)
+will install a code with a graphical user interface.
+
+On Linux you may want to use ccmake or cmake-gui. On ubuntu, these are
+available through the package manager (e.g. `sudo apt-get install ccmake`).
+
+## Dependencies
+
+### External dependencies
+
+MeshTools can optionally be built with CGAL extensions.
+It will need CGAL 4.12 (or higher).
+
+On Linux you mau just install the ad-hoc package.
+As from version 4.12, CGAL can be used in a header only mode you may also just want to download the [CGAL source code](https://github.com/CGAL/cgal/releases) and set the CMake `CGAL_DIR` variable point to the source directory.
+
+If you install MeshTools using pip (cf. below) and want to use a specific version of CGAL  don't forget to use the `MESHTOOLS_WITH_CGAL_DIR` environment variable to pass the value of `CGAL_DIR` to cmake.
+
+On Windows you may need precompiled versions of the *gmp* and *mpfr* libraries that you may obtain from the (Geometry Factory website)[https://doc.cgal.org/latest/Manual/installation.html#title5].
+
+
+### Packaged dependencies
+
+MehsTools is packaged with the following third parties that are grouped in the `thirdparties`
+directory and automatically compiled through the cmake build system :
+* [pybind11](https://pybind11.readthedocs.io/en/stable/) v.2.2.3 which is a wonderfull header only library to interface python and C++ code,
+* the [mapbox implementation](https://github.com/mapbox/variant) of the C++
+[variant](http://en.cppreference.com/w/cpp/utility/variant)
+and [optional](http://en.cppreference.com/w/cpp/utility/optional) concepts.
+
+# Building and installation
+
+## Building with setuptools
+
+If all the requirements above are accessible on your path it's possible that you can use [pip](https://pypi.org/project/pip/)
+and/or setuptools, after having set `MESHTOOLS_WITH_CGAL_DIR` environment variable pointing to where CGAL resides (in the case you want to use it).
+
+To have a system default installation just run in the root MeshTools directory:
+
+```shell
+pip install .
+```
+
+or if you want to have an *editable install*,
+wich is fundamentally a *setuptools develop mode*, that is to say that
+no files will be copied and changes to the package file
+will directly directly affect the use of the module elsewhere :
+
+```shell
+pip install -e .
+```
+
+Changes to the C/C++ source files will only be available if the code is recompiled.
+To do so, look for the build directory (it should be hidden in the
+`build` directory on the top level of the MeshTools package repository).
+From there you can use cmake to run your underlying native build tool :
+
+```shell
+cmake --build . --target install
+```
+or just open the Microsoft Visual Studio solution file,
+or if on Linux and using make :
+
+```shell
+make install
+```
+
+## Building through CMake
+
+Just follow the steps [here](https://cmake.org/runningcmake/), prefer
+*out of source build*. The source code is the directory that you have cloned using
+git, it is the top level directory where you can find a `CMakeLists.txt` file.
+
+Once the library is built you will have all of the python modules composing MeshTools in the MeshTools directory. These can be imported into python 
+scripts and used as in the example scripts in the `tests\python` directory.
+
---
+++ setup.py
@@ -0,0 +1,103 @@
+# This is adapted from https://github.com/molpopgen/cmake_example/tree/subdir_and_python
+
+import os
+import re
+import sys
+import platform
+import subprocess
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext
+from distutils.version import LooseVersion
+from multiprocessing import cpu_count
+
+class CMakeExtension(Extension):
+    def __init__(self, name, sourcedir=''):
+        # depends option does not seem to work
+        Extension.__init__(self, name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
+
+
+class CMakeBuild(build_ext):
+    
+    def run(self):
+        
+        # check that cmake is present on the path and recent enough
+        try:
+            out = subprocess.check_output(['cmake', '--version'])
+        except OSError:
+            raise RuntimeError("CMake must be installed to build the following extensions: " +
+                               ", ".join(e.name for e in self.extensions))
+
+        if platform.system() == "Windows":
+            cmake_version = LooseVersion(re.search(r'version\s*([\d.]+)', out.decode()).group(1))
+            if cmake_version < '3.1.0':
+                raise RuntimeError("CMake >= 3.1.0 is required on Windows")
+
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        
+        cmake_args = ['-DPYTHON_EXECUTABLE=' + sys.executable]
+        
+        # this is where setuptools will build the module
+        extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
+        # MESHTOOLS_PYTHON_PACKAGE_DIRECTORY is a CMake variable to set the installation prefix
+        cmake_args+= ['-DMESHTOOLS_PYTHON_PACKAGE_DIRECTORY=' + extdir]
+        # Link to CGAL directory
+        try:
+            CGAL_DIR = os.environ['MESHTOOLS_WITH_CGAL_DIR']
+            if not os.path.isdir(CGAL_DIR):
+                print('WARNING: The MESHTOOLS_WITH_CGAL_DIR environment variablereferences an non existing directory and will not be used!', file=sys.stderr)
+            else:
+                cmake_args+= ['-DCGAL_DIR=' + os.path.abspath(CGAL_DIR)]
+        except KeyError:
+            print('WARNING: CGAL extensions will not be compiled!', file=sys.stderr)
+            print('The MESHTOOLS_WITH_CGAL_DIR environment variable is used to point to the desired CGAL installation directory.', file=sys.stderr)
+        build_args = ['--target', 'install'] # we will build the install target
+        # config defaults to Debug on Windows if not passed to the build tool
+        cfg = 'Debug' if self.debug else 'Release'
+        build_args+= ['--config', cfg]
+        # '--' will pass remaining options to the native build tool
+        build_args+= ['--']
+        
+        if platform.system() == "Windows":
+            if sys.maxsize > 2**32:
+                cmake_args += ['-A', 'x64']
+            build_args += ['/m']
+        else:
+            cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg]
+            build_args += ['-j%d' % cpu_count()]
+        
+        env = os.environ.copy()
+        env['CXXFLAGS'] = '{} -DVERSION_INFO=\\"{}\\"'.format(
+            env.get('CXXFLAGS', ''),
+            self.distribution.get_version()
+        )
+        
+        # this is the build directory for compiled modules
+        build_dir = os.path.abspath(self.build_temp) + '-cmake'
+        if not os.path.exists(build_dir):
+            os.makedirs(build_dir)
+        # cmake call to set up the native build
+        cmake_cmd = ['cmake', ext.sourcedir] + cmake_args
+        subprocess.check_call(cmake_cmd, cwd=build_dir, env=env)
+        # cmake is then used to call the native build tool
+        # just re-run cmake --build . --target install
+        build_cmd = ['cmake', '--build', '.'] + build_args
+        subprocess.check_call(build_cmd, cwd=build_dir)
+
+setup(
+    name='MeshTools',
+    version='0.0.1',
+    author='brgm',
+    author_email='anr-charms@brgm.fr',
+    description='A python library for managing mesh information and passing to ComPASS.',
+    long_description='',
+    packages=['MeshTools'],
+    ext_package='MeshTools',
+    ext_modules=[ CMakeExtension('_MeshTools') ], #name (first argument) must match the name of the exported pybind11 module
+    cmdclass=dict(build_ext=CMakeBuild),
+    zip_safe=False,
+)
