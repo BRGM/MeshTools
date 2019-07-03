e087de5bc974da9c57105cfe12e01b4bbfe515a7
Simon Lopez <s.lopez@brgm.fr>
2019-07-03 10:14:40 +0200

    Add experimental features
    Partial C++ implementation of splittable elements but Python seems to be enough for the moment.

---
+++ tests/python/test-split.py
@@ -0,0 +1,16 @@
+from MeshTools.SplitManager import Splittables
+
+n = 10
+
+S = Splittables(n)
+assert all(s.is_elementary for s in S)
+
+S.split(1, (2, 3, 4))
+S.split(3, (5,6))
+S.split(6, (8,9))
+
+for i, s in enumerate(S):
+    if s.is_elementary:
+        print(i, '*')
+    else:
+        print(i, ':', S.elementary_elements(i).array)
